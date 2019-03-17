c97cb6062f9890b09d62e6bbb8d6a88c0580dc15
Arfinda Ilmania <findafn@gmail.com>
2019-03-17 22:58:08 +0700

    edit build_model for sentiment

--- sentiment/SentimentClassifier.py
+++ sentiment/SentimentClassifier.py
@@ -1,146 +1,227 @@
-import Preprocessor
-from Preprocessor import MAX_LENGTH, EMBEDDING_SIZE, ASPECTS
+from Preprocessor import Preprocessor
+from Preprocessor import MAX_LENGTH, EMBEDDING_SIZE, ASPECT_LIST
 
 import tensorflow as tf
+import numpy as np
+import json
+import os
 
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score, recall_score, precision_score, accuracy_score
 
 from keras import backend as k
+from keras.preprocessing.sequence import pad_sequences
 from keras.models import Model, load_model
 from keras.layers import Dense, Dropout, Input, Embedding, TimeDistributed
 from keras.layers import GRU, LSTM, Bidirectional, GlobalMaxPool1D, Conv1D, MaxPooling1D
 
 class SentimentClassifier():
+    config_file = 'config.json'
+    weight_file = 'model.h5'
+    result_file = 'result.txt'
+
     def __init__ (
-            self, 
-            normalize = True,
+            self,
+            normalize = False,
             lowercase = True,
             remove_punct = True,
-            max_length = max_lenth,
-            masking = True,
+            masking = False,
             embedding = True,
-            embedding_size = embedding_size,
             trainable_embedding = True,
             pos_tag = None,
+            dependency = None,
             use_rnn = True,
             rnn_type = 'lstm',
+            return_sequence = True,
             use_cnn = False,
+            use_svm = False,
+            use_stacked_svm = False,
             use_attention = False,
             n_neuron = 128,
             n_dense = 1,
             dropout = 0.5,
-            optimizer = 'adam'):
+            regularizer = None,
+            optimizer = 'adam',
+            learning_rate = 0.001,
+            weight_decay = 0):
         self.preprocessor  =  Preprocessor(
             normalize = normalize,
             lowercase = lowercase,
             remove_punct = remove_punct,
-            max_length = max_length,
-            masking = masking
-        )
+            masking = masking,
+            embedding = embedding,
+            pos_tag = pos_tag,
+            dependency = dependency
+        )        
         self.tokenizer = self.preprocessor.get_tokenized()
-        self.aspects = aspects
+        self.aspects = ASPECT_LIST
         self.model = None
         self.history = None
+        self.result = None
 
         self.embedding = embedding
-        self.embedding_size = embedding_size
         self.trainable_embedding = trainable_embedding
         self.pos_tag = pos_tag
+        self.dependency = dependency
         self.use_rnn = use_rnn
         self.rnn_type = rnn_type
         self.use_cnn = use_cnn
+        self.use_svm = use_svm
+        self.use_stacked_svm = use_stacked_svm
         self.use_attention = use_attention
-        self.n_neuron = n_neuron
-        self.n_dense = n_dense
+        self.n_neuron = n_neuron 
+        self.n_dense = n_dense 
         self.dropout = dropout
+        self.regularizer = regularizer
         self.optimizer = optimizer
+        self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
+
+        print("Object has been created")
+
+    def __get_config(self):
+        keys = [
+            'embedding',
+            'trainable_embedding',
+            'pos_tag',
+            'dependency',
+            'use_rnn',
+            'rnn_type',
+            'use_cnn',
+            'use_svm',
+            'use_stacked_svm',
+            'use_attention',
+            'n_neuron',
+            'n_dense',
+            'dropout',
+            'regularizer',
+            'optimizer',
+            'learning_rate',
+            'weight_decay',
+            'batch_size',
+            'epochs',
+            'verbose',
+            'validation_split',
+            'cross_validation',
+            'n_fold',
+            'grid_search',
+            'callbacks',
+        ]
+        return {k: getattr(self, k) for k in keys}
 
     def __build_model(self):
-        vocab_size = self.preprocessor.get_vocab_size()
+        print("Building the model...")
+        vocab_size = self.preprocessor.get_vocab_size(self.tokenizer)
 
         if self.embedding:
             embedding_matrix = self.preprocessor.get_embedding_matrix(self.tokenizer)
-            main_input = Input(shape=(self.max_length,), dtype='int32', name='main_input')
+            main_input = Input(shape=(MAX_LENGTH,), dtype='int32', name='main_input')
             x = Embedding(
-                output_dim=self.embedding_size, 
-                input_dim=vocab_size, 
-                input_length=self.max_length,
+                output_dim=EMBEDDING_SIZE,
+                input_dim=vocab_size,
+                input_length=MAX_LENGTH,
                 weights=[embedding_matrix],
                 trainable=self.trainable_embedding,
             )(main_input)
 
             if self.pos_tag is 'embedding':
                 pos_matrix = self.preprocessor.get_pos_matrix(review)
-                pos_input = Input(shape=(self.max_length,), dtype='int32', name='pos_input')
+                pos_input = Input(shape=(MAX_LENGTH,), dtype='int32', name='pos_input')
                 x2 = Embedding(
-                    output_dim=30, 
-                    input_dim=pos_size, 
-                    input_length=self.max_length,
+                    output_dim=30,
+                    input_dim=pos_size,
+                    input_length=MAX_LENGTH,
                     weights=[pos_matrix],
                     trainable=self.trainable_embedding,
                 )(pos_input)
                 x = keras.layers.concatenate([x, x2])
 
         else:
-            embedded_input = self.preprocessor.get_embedded_input(review)
+            new_embedding_size = EMBEDDING_SIZE
+            if self.pos_tag is 'one_hot':
+                new_embedding_size += 27
+            if self.dependency is True:
+                new_embedding_size += 2
+            print('embedding size: ', new_embedding_size)
+            main_input = Input(shape=(MAX_LENGTH, new_embedding_size), name='main_input')
 
-        if self.pos_tag is 'one_hot':
-            encoded_pos = self.preprocessor.get_encoded_pos(review)
-
+        print("1. Input")
+        
         if self.use_rnn is True:
-            if rnn_type is 'gru':
-                x = Bidirectional(GRU(self.n_neuron, return_sequences=True))(x)
+            if self.embedding is True:
+                if self.rnn_type is 'gru':
+                    x = Bidirectional(GRU(self.n_neuron, return_sequences=True))(x)
+                else:
+                    x = Bidirectional(LSTM(self.n_neuron, return_sequences=True))(x)
             else:
-                x = Bidirectional(LSTM(self.n_neuron, return_sequences=True))(x)
+                if self.rnn_type is 'gru':
+                    x = Bidirectional(GRU(self.n_neuron, return_sequences=True))(main_input)
+                else:
+                    x = Bidirectional(LSTM(self.n_neuron, return_sequences=True))(main_input)
             x = GlobalMaxPool1D()(x)
             x = Dropout(self.dropout)(x)
 
+        print("2. LSTM")
+
         if self.use_cnn is True:
-            continue
+            pass
 
         if self.n_dense is not 0:
             for i in range(self.n_dense):
                 x = Dense(self.n_neuron, activation='relu')(x)
                 x = Dropout(self.dropout)(x)
 
-        out = Dense(len(self.aspects), activation='sigmoid')(x)
+        print("3. Dense")
+
+        out = Dense(2, activation='softmax')(x)
+
+        print("4. Out")
 
         if self.pos_tag is 'embedding':
             model = Model([main_input, pos_input], out)
         else:
             model = Model(main_input, out)
 
+        print("5. Model")
+
         model.summary()
         model.compile(
-            loss='binary_crossentropy', 
-            optimizer=self.optimizer, 
-            metrics='acc'
+            loss='categorical_crossentropy',
+            optimizer=self.optimizer,
+            metrics=['acc']
         )
+
+        print("6. Done")
 
         return model
 
     def train(
-        self, 
-        x_train, 
+        self,
+        x_train,
         y_train,
         batch_size = 16,
-        epochs = 10,
+        epochs = 5,
         verbose = 1,
         validation_split = 0.0,
         cross_validation = False,
         n_fold = 3,
-        callbacks = None
-        ):
+        grid_search = False,
+        callbacks = None):
 
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.verbose = verbose
+        self.validation_split = validation_split
+        self.cross_validation = cross_validation
+        self.n_fold = n_fold
+        self.grid_search = grid_search
+        self.callbacks = callbacks
+        
         model = self.__build_model()
 
-        if self.pos_tag is 'embedding':
-            x_input = [x_train, pos_tag]
-        else: 
-            x_input = x_train
+        print("Training...")
 
-        history = self.model.fit(
-            x = x_input, 
+        history = model.fit(
+            x = x_train, 
             y = y_train, 
             batch_size = batch_size,
             epochs = epochs, 
@@ -152,12 +233,13 @@ class SentimentClassifier():
         self.model = model
         self.history = history
 
-    def evaluate(self, x_train, x_test, y_train, y_test):
+    def evaluate(self, x_train, y_train, x_test, y_test):
         x = [x_train, x_test, y_train, y_test]
-        x_pos = [pos_train, pos_test]
-        x_name = ['TRAIN', 'TEST']
+        # x_pos = [pos_train, pos_test]
+        x_name = ['Train-All', 'Test-All']
 
-        print("=============== EVALUATION ===============")
+        print("======================= EVALUATION =======================")
+        print('{:10s} {:10s} '.format('ASPECT', 'ACC'))
 
         for i in range(2):
             if self.pos_tag is 'embedding':
@@ -173,16 +255,15 @@ class SentimentClassifier():
             y_true = (y_true>0).astype(int)
 
             acc = accuracy_score(y_true.reshape([-1]), y_pred.reshape([-1]))
-            precision = precision_score(y_true, y_pred, average='micro')
-            recall = recall_score(y_true, y_pred, average='micro')
-            f1 = f1_score(y_true, y_pred, average='micro')
 
-            print(x_name[i])
-            print('Akurasi : ', acc, '\n')
+            self.result = {
+                'pred' : y_pred,
+                'true' : y_true
+            }
+
+            print('{:10s} {:<10.4f} '.format(x_name[i], acc))
 
     def evaluate_each_aspect(self, x_test, y_test):
-        print("=============== EVALUATION FOR EACH ASPECT ===============")
-
         if self.pos_tag is 'embedding':
             y_pred = self.model.predict([x_test, pos_test])
         else:
@@ -198,7 +279,7 @@ class SentimentClassifier():
             tmp = list()
             tmp = [item[i] for item in y_true]
             y_truelist.append(tmp)
-
+                
         for i in range(len(self.aspects)):
             y_predlist[i] = np.asarray(y_predlist[i])
             y_truelist[i] = np.asarray(y_truelist[i])
@@ -207,24 +288,53 @@ class SentimentClassifier():
             y_truelist[i] = (y_truelist[i]>0).astype(int)
 
             acc = accuracy_score(y_truelist[i], y_predlist[i])
-            print(self.aspects[i], acc)
+            print('{:10s} {:<10.4f} '.format(self.aspects[i], acc))
+
+        print('\n')
 
     def predict(self, new):
         encoded_new = self.tokenizer.texts_to_sequences([new.lower()])
-        input_new = pad_sequences(encoded_new, maxlen=self.max_length, padding='post')
+        input_new = pad_sequences(encoded_new, maxlen=MAX_LENGTH, padding='post')
         pred = self.model.predict(input_new)
-
+        
         label = list()
         for i in range(len(pred)):
             for j in range(len(pred[i])):
                 if (pred[i][j] > 0.5):
-                    label.append(aspects[j])
+                    label.append(self.aspects[j])
 
+        print("======================= PREDICTION =======================" )
+        print(new)
+        print(label)
         return label
 
-    def save(self, file_path):
-        self.model.save(file_path)
+    def save(self, dir_path):
+        if not os.path.exists(dir_path):
+            print("Making the directory: {}".format(dir_path))
+            os.mkdir(dir_path)
+        self.model.save(os.path.join(dir_path, self.weight_file))
+        y = self.__get_config()
+        with open(os.path.join(dir_path, self.config_file), 'w') as f:
+            json.dumps(y, indent=4, sort_keys=True)
 
-    def load(self, file_path):
-        sentimentModel = load_model(file_path)
-        return sentimentModel
+        review_test = self.preprocessor.read_data_for_sentiment('sentiment/data/sentiment_test.json')
+
+        # with open(os.path.join(dir_path, self.result_file), 'w') as f:
+        #     for i, pred in enumerate(self.result['pred']):
+        #         f.write(review_test[i])
+        #         temp = list()
+        #         true = list()
+        #         for j, asp in enumerate(pred):
+        #             if asp == 1:
+        #                 temp.append(self.aspects[j])
+        #             if self.result['true'] == 1:
+        #                 true.append(self.aspects[j])
+        #         f.write(json.dump(temp) + "\n")
+        #         f.write(json.dump(true) + "\n")
+
+    def load(self, dir_path, custom={'f1':f1}):
+        if not os.path.exists(dir_path):
+            raise OSError('Directory \'{}\' not found.'.format(dir_path))
+        else:
+            aspectModel = load_model(os.path.join(dir_path, self.weight_file), custom_objects=custom)
+        return aspectModel
